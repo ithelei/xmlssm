b29b292de131f040c98d3bccfd994f1f6f4fa7ba
ithelei <ithelei@sina.cn>
2019-09-03 23:25:04 +0800

    xmlssm idea传统xml配置方式

---
+++ src/main/java/org/javaboy/service/HelloService.java
@@ -0,0 +1,13 @@
+package org.javaboy.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String  sayhello(){
+
+     return "ithelei";
+    }
+}
